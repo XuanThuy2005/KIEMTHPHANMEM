c88334013bb71b3bc68e10a41d0174b6e5989b40
XuanThuy2005 <148856223+XuanThuy2005@users.noreply.github.com>
2026-01-07 15:37:37 +0700

    Create StudentAnalyzerTest.java

---
+++ unit-test/test/StudentAnalyzerTest.java
@@ -0,0 +1,30 @@
+package org.yourcompany.yourproject;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class StudentAnalyzerTest {
+
+    @Test
+    public void testCountExcellentStudents() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(2,
+            analyzer.countExcellentStudents(Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0))
+        );
+        assertEquals(0,
+            analyzer.countExcellentStudents(Collections.emptyList())
+        );
+    }
+
+    @Test
+    public void testCalculateValidAverage() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(8.17,
+            analyzer.calculateValidAverage(Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)),
+            0.01
+        );
+    }
+}
